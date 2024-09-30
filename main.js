7571e103c28c5d37585d90e2d9e01b8958098343
idko2004 <ivancito72011@gmail.com>
2024-09-29 20:13:03 -0400

    debería de saber cuando tiene poca batería

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
 let UPDATE_RATE_IN_MS = 3_000;
-//let UPDATE_RATE_IN_MS = process.env.UPDATE_RATE_IN_MS || 1_800_000; //30 minutos
+let BATTERY_PERCENTAGE_THRESHOLD = process.env.BATTERY_PERCENTAGE_THRESHOLD || 30;
+//let UPDATE_RATE_IN_MS = process.env.UPDATE_RATE_IN_MS || 3_600_000; //1 hora
 
 const {exec} = require('child_process');
 
-getBatteryInformation();
+function sendEmail(percentage)
+{
+	console.log(`The device is at ${percentage}% of charge, please plug it in.`);
+}
 
-function getBatteryInformation()
+async function monitorBattery()
+{
+	const status = await getBatteryStatus();
+	if(status == undefined)
+	{
+		console.log("[ERROR] monitorBattery: Failed to get battery status.");
+		return;
+	}
+
+	if(status.percentage <= BATTERY_PERCENTAGE_THRESHOLD
+	&& status.plugged == "UNPLUGGED")
+	{
+		sendEmail(status.percentage);
+	}
+	else
+	{
+		console.log(`[INFO] Device is at ${status.percentage}% of charge.`);
+	}
+}
+
+function getBatteryStatus()
 {
 	return new Promise((good, bad) =>
 	{
 		exec("termux-battery-status", (stderr, stdout) =>
 		{
-			console.log("stderr: ", stderr);
-			console.log("stdout: ", stdout);
+			//console.log("stderr: ", stderr);
+			//console.log("stdout: ", stdout);
+
+			if(stderr != null)
+			{
+				console.log("[WARN] getBatteryStatus: Something appeared on stderr, Printing:\n", stderr);
+			}
 		
 			try
 			{
@@ -20,9 +49,16 @@ function getBatteryInformation()
 			}
 			catch(err)
 			{
-				console.log("[ERROR] getBatteryInformation: Failed to parse as JSON. Printing what I was trying to parse:\n", stdout);
+				console.log("[ERROR] getBatteryStatus: Failed to parse as JSON. Printing what I was trying to parse:\n", stdout);
 				bad(undefined);
 			}
 		});
 	})
 }
+
+setInterval(() => 
+{
+	monitorBattery();
+}, UPDATE_RATE_IN_MS);
+
+monitorBattery();
